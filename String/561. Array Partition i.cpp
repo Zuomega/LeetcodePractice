8b44fd8f9aab92ab85374464e6873d751906ff83
Zuomega <25094987+Zuomega@users.noreply.github.com>
2022-06-06 07:14:16 +0800

    add two

---
+++ String/561. Array Partition i.cpp
@@ -0,0 +1,25 @@
+/*
+给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从 1 到 n 的 min(ai, bi) 总和最大。
+
+返回该 最大总和 。
+
+ 
+
+作者：力扣 (LeetCode)
+链接：https://leetcode.cn/leetbook/read/array-and-string/c24he/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i+=2)
+        {
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
